4e8acae1b8eb753db97afb7850b889a681c902ca
孙珊珊 <578986921@qq.com>
2018-07-10 10:32:41 +0800

    前端布局

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,3 +1,13 @@
 <?php
 
+if (! function_exists('route_class')) {
+    /**
+     * 将路由转换成页面 div class 名称
+     * @return void
+     */
+    function route_class()
+    {
+        return str_replace('.', '-', \Route::currentRouteName());
+    }
+}
 ?>
