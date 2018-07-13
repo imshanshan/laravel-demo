1986a3611d026db656b5150ed1c5fc415757ebdd
孙珊珊 <578986921@qq.com>
2018-07-13 10:03:55 +0800

    注册验证

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //Blade::component('components.alert', 'alert');
-        Blade::withoutDoubleEncoding();
+       // Blade::withoutDoubleEncoding();
+       Carbon::setLocale('zh');
     }
 
     /**
